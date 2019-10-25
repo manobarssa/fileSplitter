82df207460b618d91d6c9a78eb70d9d8e12b549e
Manobarssa <30561698+manobarssa@users.noreply.github.com>
2019-10-24 23:59:26 -0300

    Add files via upload

--- fileSplitter.py
+++ fileSplitter.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct 18 22:55:23 2019
+version 0.3
 
-@author: Anderson
+@author: @manobarssa
 """
 #!python3
 
@@ -11,47 +11,81 @@ import argparse, os
 def splitter(path, chunk):    
     with open(path,'rb') as f:
         fb = f.read(chunk)
-        count = 0    
+        count = 0
+        basename = os.path.basename(path)
+        os.mkdir(basename+'_SPLITED')
+        os.chdir(basename+'_SPLITED')
         while len(fb) > 0:
-            if str(count) < 10:
+            if count < 10:
                 cname = '0' + str(count)
             else:
-                cname = str(count)
-            basename = os.path.basename()
+                cname = str(count)            
             with open(basename+'_part_' + cname, 'wb') as rsz:
                 rsz.write(fb)
             fb = f.read(chunk)
             count+=1
+        os.chdir('..')
+        return print('File parts written to {} folder.'.format(basename+'_SPLITED'))
 
-def joiner():
-    
-    files = [x for x in os.listdir() if os.path.isfile(x)]
-    filename = files[0].replace()
+def joiner(files):
+    fn = files[0]
+    filename = fn.replace(fn[fn.index('_part'):],'')
     with open(filename,'wb') as jf:
         for f in files:
             with open(f,'rb') as pf:
                 bobj = pf.read()        
             jf.write(bobj)
+    return print('Files joined to {} file.'.format(filename))
     
+def main():
+    parser = argparse.ArgumentParser(description= ''' 
+    Split a file into n parts or join n parts into a file.''')
+        
+    #Chunk or n group arguments
+    chorn = parser.add_mutually_exclusive_group()
+    chorn.add_argument('-c', dest='chunk', metavar='Chunk Size', type=int,
+                       help='Chunk size in bytes.')
+    chorn.add_argument('-n', dest='nparts', metavar='n_parts', type=int,
+                       help='Number of parts in which the file will be splited.')
+    
+    #General arguments    
+    parser.add_argument('-f', dest='file', metavar='File Path', type=str, 
+                        help='Path to file that will be splited.')
+    parser.add_argument('-m', dest='mode', metavar='Mode', type=str,
+                        help='Splitter (s) or Joiner (j).', required=True)
+    
+            
+    args = parser.parse_args()
+    #Defines the chunk size
+    if args.chunk is not None:
+        chunk = args.chunk
+    elif args.nparts is not None:
+        n = args.n_parts
+        file_size = os.path.getsize(args.file)
+        chunkInt = file_size//n
+        chunkDec = (file_size/n) - (chunkInt)
+        if chunkDec == 0:
+            chunk = chunkInt
+        else:
+            chunk = chunkInt + 1        
+    
+    #Splits the file    
+    if args.mode.lower() == 's':
+        path = args.file        
+        splitter(path,chunk)
+    else:
+        files = [x for x in os.listdir() if os.path.isfile(x)]
+        joiner(files)
 
         
-    if '-n' in args_dict.keys():
-        file_size = os.path.getsize(path)
-        n = int(args_dict['-n'])
-        blockInt = file_size//n
-        blockDec = (file_size/n) - (blockInt)
-        if blockDec == 0:
-            block = blockInt
-        else:
-            block = blockInt + 1
-        
-        with open(path,'rb') as f:
-            fb = f.read(block)
-            count = 0    
-            while len(fb) > 0:
-                with open(basename+'_part_' + str(count), 'wb') as rsz:
-                    rsz.write(fb)
-                fb = f.read(block)
-                count+=1
+if __name__ == '__main__':
+    main()    
+
+
+
     
-    print('Partitioned files has been written to {}'.format(os.getcwd()))
+    
+        
+        
+    
+    
